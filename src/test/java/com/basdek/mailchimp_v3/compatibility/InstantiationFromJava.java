1b8116d013b1c1adc1ef4986ece7a24e93ea8407
Sebastiaan de Kwant <basdekwant@gmail.com>
2016-02-11 15:27:46 +0100

    Made MailChimpList java friendlier

--- src/test/java/com/basdek/mailchimp_v3/compatibility/InstantiationFromJava.java
+++ src/test/java/com/basdek/mailchimp_v3/compatibility/InstantiationFromJava.java
@@ -1,5 +1,8 @@
 package com.basdek.mailchimp_v3.compatibility;
 
+import com.basdek.mailchimp_v3.dto.MailChimpList;
+import com.basdek.mailchimp_v3.dto.MailChimpList_CampaignDefaults;
+import com.basdek.mailchimp_v3.dto.MailChimpList_Contact;
 import com.basdek.mailchimp_v3.dto.MailChimpMember;
 
 public class InstantiationFromJava {
@@ -8,6 +11,20 @@ public class InstantiationFromJava {
         return MailChimpMember.build(
             "minerva.mcgonagall@basdek.com", "html", "subscribed",
             null, null, "Dutch", false, null
+        );
+    }
+
+    public MailChimpList_Contact instantiateMailChimpList_Contact() {
+        return MailChimpList_Contact.build(
+            "company", "addr", null, "city", "state", "zip", "country", null
+        );
+    }
+
+    public MailChimpList instantiateMailChimpList() {
+        return MailChimpList.build(
+            "testlist", this.instantiateMailChimpList_Contact(), "perm reminder",
+            true, new MailChimpList_CampaignDefaults("from", "from@basdek.com",
+            "subject", "EN"), null, null, false, null
         );
     }
 
